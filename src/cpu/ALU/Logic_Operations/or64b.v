3510fce48174288c22d209bb1a304f6764b327ce
00SRPK <102863497+00SRPK@users.noreply.github.com>
2023-04-30 13:04:02 -0300

    Update src/cpu/ALU/Logic_Operations/or64b.v
    Co-authored-by: Vitor Sasaki <63128590+vSasakiv@users.noreply.github.com>

--- src/cpu/ALU/Logic_Operations/or64b.v
+++ src/cpu/ALU/Logic_Operations/or64b.v
@@ -1,5 +1,5 @@
-/* Módulo OR de 32 bits.
-Faz um bitwise OR entre dois valores de 32 bits, A e B.
+/* Módulo OR de 64 bits.
+Faz um bitwise OR entre dois valores de 64 bits, A e B.
 Resultado vai para saída O. */
 module or64b (
     input [63:0] a, b,
