691e94cca19c276123b658f47eb362a6113ce10d
00SRPK <102863497+00SRPK@users.noreply.github.com>
2023-04-30 13:03:06 -0300

    Update src/cpu/ALU/Logic_Operations/xor64b.v
    Co-authored-by: Vitor Sasaki <63128590+vSasakiv@users.noreply.github.com>

--- src/cpu/ALU/Logic_Operations/xor64b.v
+++ src/cpu/ALU/Logic_Operations/xor64b.v
@@ -1,5 +1,5 @@
-/* Módulo XOR de 32 bits.
-Faz um bitwise XOR entre dois valores de 32 bits, A e B.
+/* Módulo XOR de 64 bits.
+Faz um bitwise XOR entre dois valores de 64 bits, A e B.
 Resultado vai para saída Xor. */
 module xor64b (
     input [63:0] a, b,
