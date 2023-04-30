9b8526a41b1fe700c97ac1d5aa6e2b7491ec158c
00SRPK <102863497+00SRPK@users.noreply.github.com>
2023-04-30 12:02:44 -0300

    Update comparator_lt_unsigned.v

--- src/cpu/ALU/Comparators/comparator_lt_unsigned.v
+++ src/cpu/ALU/Comparators/comparator_lt_unsigned.v
@@ -1,12 +1,12 @@
 /* 
 Módulo para realização de comparação entre dois números
-unsigned A e B, devolvendo R = 1 caso A < B,
+unsigned A e B, devolvendo lu = 1 caso A < B,
 dados o Carry out da soma pelo complemento de dois
 do segundo (subtração) e a igualdade.
  */
 module comparator_lt_unsigned (
-  input wire c_o, eq, // Entradas COUT e igualdade
-  output wire lu // Saída
+  input wire c_o, eq, // Entradas c_o (carry out) e eq (igualdade)
+  output wire lu 
 );
 
   assign lu = ~(c_o | eq); // Expressão lógica derivada de tabela verdade
