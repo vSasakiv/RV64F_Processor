9aba33efefa247676a8a06de1962c396fcb1b279
00SRPK <102863497+00SRPK@users.noreply.github.com>
2023-04-30 12:18:07 -0300

    Update gate.v

--- src/cpu/Control_Unit/DECODERS/gate.v
+++ src/cpu/Control_Unit/DECODERS/gate.v
@@ -2,12 +2,12 @@
 que deveram passar dos decodificados para o resto do circuito,
 onde os sinais "code" vem dos OPDecoders, e Dec_Data vem do sinal concatenado dos decodificados */
 module gate (
-  input wire [9:0] code, Dec_Data,
+  input wire [9:0] code, dec_data,
   output wire S
 );
 
   // Como cada code possui 0 em 9 bits e 1 em apenas um (Ex: 10'b0010000000),
   // basta fazer um bitwise and com cada bit do code e do dec_data, e em seguida fazer um or com cada bit para obter o resultado.
-  assign S = |(code & Dec_Data);
+  assign S = |(code & dec_data);
 
 endmodule
