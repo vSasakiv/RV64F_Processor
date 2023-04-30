38a6a31bef8e93155f8342c5383a0677e320e63f
00SRPK <102863497+00SRPK@users.noreply.github.com>
2023-04-30 13:04:36 -0300

    Update src/cpu/ALU/Comparators/comparator.v

--- src/cpu/ALU/Comparators/comparator.v
+++ src/cpu/ALU/Comparators/comparator.v
@@ -3,7 +3,7 @@ Módulo que junta todos os 3 comparadores feitos
 em um único módulo 
 */
 module comparator (
-  input wire a_sign, b_sign, c_o, // Bit de sinal de A e B, e o bit COUT
+  input wire a_sign, b_sign, c_o, // Bit de sinal de a e a, e o bit c_o (carry out)
   input wire [63:0] s, // Valor da subtração A - B
   output wire eq, lu, ls // Saídas das comparações
 );
