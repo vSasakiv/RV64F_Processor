0000f16f0d53b19b6f2bb9aaefcb2a14dfd04d9a
Vinicius <viniciusbsilva@usp.br>
2023-05-20 16:47:24 -0300

    :rocket: dataflow

--- src/cpu/DataFlow/dataflow.v
+++ src/cpu/DataFlow/dataflow.v
@@ -1,62 +1,205 @@
-/* Módulo que contém a junção de todos os módulos característicos do dataflow */
+/* Módulo que contém a junção de todos os módulos pertecentes ao dataflow */
 module dataflow (
-  input sub_sra, // sub sra alu
-  input insn_clk, pc_clk, rd_clk, // Sinais de clock gerados na CU
-  input reset, // reset
-  input pc_next_sel, // seletor pc next
-  input pc_alu_sel, // seletor pc alu
-  input alu_sel_a, alu_sel_b, // seletor alu
-  input addr_sel, // seletor de endereço para memória RAM
-  input [1:0] rd_sel, // seletor mux para RD
-  input [2:0] func, mem_extend, // func e memory extension size
-  input [4:0] rs1, rs2, rd, // endereços dos registradores de entrada e saída 
-  input [31:0] data_o, // saída da memória
-  input [31:0] imm, // valor do imediato
-  output [31:0] addr, // endereço para acesso de memória RAM
-  output [31:0] insn_out, // saída contendo instrução
-  output [31:0] rs2_mem_i, // saída rs2 para memória
-  output EQ, LS, LU // saídas de comparação
+  input clk,
+  input sub_sra,
+  input write_mem,
+  input reset,
+  input sel_pc_next, sel_pc_alu, sel_alu_a, sel_alu_b,
+  input load_ins, load_imm, load_regfile, load_pc, load_rs1, load_rs2, load_alu, load_pc_alu, load_data_memory,
+  input [1:0] sel_rd, sel_mem_size,
+  input [2:0] func3,
+  input [2:0] sel_mem_extension,
+  input [4:0] rd_addr, rs1_addr, rs2_addr,
+  input [31:0] code,
+  output [2:0] flags_value;
+  output [31:0] insn
 );
+  wire eq, ls, lu;
+  wire [31:0] insn_o, insn_value;
+  wire [63:0] imm_o, imm_value;
+  wire [63:0] alu_o, alu_value;
+  wire [63:0] alu_a_value, alu_b_value;
+  wire [63:0] mem_extended, mem_o, mem_value;
+  wire [63:0] rd_i, rs1_o, rs2_o, rs1_value, rs2_value;
+  wire [63:0] pc_alu_o, pc_alu_value, pc_selected, pc_alu_selected, pc_value;
 
-  wire [31:0] mem_extended; // saída do memory extender
-  wire [31:0] insn; // instrução
-  wire [31:0] alu_val; // saída da alu
-  wire [31:0] pc; // net que contém o valor atual do program counter (PC)
-  wire [31:0] pc_inc; // net que contém o valor do program counter somado com a entrada do alu_pc
-  wire [31:0] pc_selected; // valor que deverá ser o próximo PC, podendo ser um valor da ALU principal ou da própria alu do PC
-  wire [31:0] rd_val; // valor que será gravado em um registrador do Regfile
-  wire [31:0] pc_alu_selected;  // saída da ALU do PC
-  wire [31:0] rs1_val; // valor de saída rs1 do regfile
-  wire [31:0] rs2_val; // valor de saída rs2 do regfile
-  wire [31:0] alu_val_a, alu_val_b; // valores a e b na entrada da ALU principal
+  assign insn = insn_value;
+  
+  //Módulo memory extender, utilizado para corrigir o valor que será carregado em um registrador do regfile, de acordo com a instrução
+  mem_extension mem_ex (
+    .sel_mem_extension,
+    .mem_value,
+    .mem_extended
+  );
 
+  //Módulo que forma o valor do imediato a partir da instrução 
+  imm_gen imm_gen (
+    .insn(insn_value),
+    .code,
+    .imm(imm_o)
+  );
 
-// módulo memory extender, utilizado para corrigir o valor que será carregado a um registrador da Regfile de acordo com a instrução
-  memsx M0 (.mem_value(data_o), .mem_extended(mem_extended), .mem_extend(mem_extend));
-  // registrador que contém as instruções sendo atualmente executadas
-  reg32bit INSNreg (.clk(insn_clk), .rs_i(reset), .data_i({data_o[7:0], data_o[15:8], data_o[23:16], data_o[31:24]}), .data_o(insn));
-  // multiplexador para selecionar o próximo valor do PC
-  mux2to1 PCNEXTSEL (.select(pc_next_sel), .I0(pc_inc), .I1(alu_val), .data_o(pc_selected));
-  // registrador para o Program Counter (PC)
-  reg32bit PCreg (.clk(pc_clk), .rs_i(reset), .data_i(pc_selected), .data_o(pc));
-  // ALU do Program Counter, neste caso sendo apenas um somador de 32bits.
-  adder32b PCADD (.A(pc), .B(pc_alu_selected), .SUB(1'b0), .S(pc_inc));
+  //Memória de instruções
+  insn_memory insn_mem (
+    .addr(pc_value),
+    .insn(insn_o)
+  );
+
+  //Memória de dados
+  data_memory data_mem (
+    .clk         (clk),
+    .write       (write_mem),
+    .sel_mem_size(sel_mem_size),
+    .data_i      (rs2_value),
+    .data_o      (mem_o),
+    .addr        (alu_value)
+  );
+
+  //Registrador para o Program Counter (PC)
+  reg_async_reset #(.Size(64)) pc (
+    .clk   (clk),
+    .reset (reset),
+    .load  (load_pc),
+    .data_i(pc_selected),
+    .data_o(pc_value)
+  );
+
+  //Registrador que guarda a saída da memória de dados
+  register #(.Size(64)) reg_data_mem (
+    .clk   (clk),
+    .load  (load_data_memory),
+    .data_i(mem_o),
+    .data_o(mem_value)
+  );
+
+  //Registrador que guarda a saída da alu do program counter
+  register #(.Size(64)) pc_alu (
+    .clk   (clk),
+    .load  (load_pc_alu),
+    .data_i(pc_alu_o),
+    .data_o(pc_alu_value)
+  );
+
+  //Registrador que guarda o imm 
+  register #(.Size(64)) reg_imm (
+    .clk   (clk),
+    .load  (load_imm),
+    .data_i(imm_o),
+    .data_o(imm_value)
+  );
+
+  //Registrador que contém as instruções sendo atualmente executadas
+  register #(.Size(32)) ir (
+    .clk,
+    .load  (load_ins),
+    .data_i(insn_o),
+    .data_o(insn_value)
+  );
+
+  //Registrador que guarda a saída rs1 do regfile
+  register #(.Size(64)) reg_alu_a (
+    .clk   (clk),
+    .load  (load_rs1),
+    .data_i(rs1_o),
+    .data_o(rs1_value)
+  );
+
+  //Registrador que guarda a saída rs2 do regfile
+  register #(.Size(64)) reg_alu_b (
+    .clk   (clk),
+    .load  (load_rs2),
+    .data_i(rs2_o),
+    .data_o(rs2_value)
+  );
+
+  //Registrador que guarda a saída da ALU
+  register #(.Size(64)) reg_alu_o (
+    .clk   (clk),
+    .load  (load_alu),
+    .data_i(alu_o),
+    .data_o(alu_value)
+  );
+
+  //Registrador que guarda as flags obtidas da ALU
+  register #(.Size(3)) reg_flags (
+    .clk   (clk),
+    .load  (load_flags),
+    .data_i({eq, ls, lu}),
+    .data_o({flags_value})
+  );
+
+  //Regfile
+  regfile #(.Size(64)) regfile (
+    .clk,
+    .load(load_regfile),
+    .rd_addr,
+    .rs1_addr,
+    .rs2_addr,
+    .rd_i,
+    .rs1_o,
+    .rs2_o
+  );
+
+  //Multiplexador para selecionar o próximo valor do PC
+  mux_2to1 #(.Size(64)) mux_pc_next  (
+    .sel   (sel_pc_next),
+    .i0    (pc_alu_o),
+    .i1    (alu_value),
+    .data_o(pc_selected)
+  );
+
   // multiplexador para seleção da segunda entrada da ALU do Program Counter
-  mux2to1 PCALUSEL (.select(pc_alu_sel), .I0(32'h00000004), .I1(imm), .data_o(pc_alu_selected));
-  // multiplexador para selecionar qual valor deverá ser gravado no registrador destino presente na Regfile
-  mux4to1 RDSEL (.select(rd_sel), .I0(mem_extended), .I1(imm), .I2(alu_val), .I3(pc_inc), .data_o(rd_val));
-  // Regfile
-  regfile REG (.clk(rd_clk), .rs_i(reset), .rd(rd), .rs1(rs1), .rs2(rs2), .rd_in(rd_val), .rs1_out(rs1_val), .rs2_out(rs2_val));
-  // multiplexador para selecionar qual valor irá entrar na ALU geral, podendo ser o PC ou o valor do rs1
-  mux2to1 ALUASEL (.select(alu_sel_a), .I0(rs1_val), .I1(pc), .data_o(alu_val_a));
-  // multiplexador para selecionar qual valor irá entrar na ALU geral, podendo ser um imediato ou o valor do rs2
-  mux2to1 ALUBSEL (.select(alu_sel_b), .I0(rs2_val), .I1(imm), .data_o(alu_val_b));
-  // ALU
-  alu ALU0 (.A(alu_val_a), .B(alu_val_b), .func(func), .sub_sra(sub_sra), .alu_val(alu_val), .EQ(EQ), .LS(LS), .LU(LU));
-  // multiplexador para selecionar endereço, utilizado em função de store para gravar algum valor.
-  mux2to1 ADDRSEL (.select(addr_sel), .I0(pc), .I1(alu_val), .data_o(addr));
+  mux_2to1  #(.Size(64)) mux_pc_alu (
+    .sel   (sel_pc_alu),
+    .i0    (64'h00000004),
+    .i1    (imm_value),
+    .data_o(pc_alu_selected)  
+  );
 
-  assign rs2_mem_i = rs2_val;
-  assign insn_out = insn;
+  // multiplexador para selecionar qual valor irá entrar na ALU geral, podendo ser o PC ou o valor do rs1
+  mux_2to1 #(.Size(64)) mux_alu_a (
+    .sel   (sel_alu_a),
+    .i0    (rs1_value),
+    .i1    (pc_value),
+    .data_o(alu_a_value) 
+  );
+
+  // multiplexador para selecionar qual valor irá entrar na ALU geral, podendo ser um imediato ou o valor do rs2
+  mux_2to1 #(.Size(64)) mux_alu_b (
+    .sel   (sel_alu_b),
+    .i0    (rs2_value),
+    .i1    (imm_value),
+    .data_o(alu_b_value) 
+  );
+
+  // multiplexador para selecionar qual valor deverá ser gravado no registrador destino presente na Regfile
+  mux_4to1 #(.Size(64)) mux_rd (
+    .sel   (sel_rd),
+    .i0    (mem_extended),
+    .i1    (imm_value),
+    .i2    (alu_value),
+    .i3    (pc_alu_value),
+    .data_o(rd_i)  
+  );
+  
+  // ALU do Program Counter, neste caso sendo apenas um somador de 64bits.
+  adder64b pc_adder (
+    .a  (pc_value),
+    .b  (pc_alu_selected),
+    .sub(1'b0),
+    .s  (pc_alu_o)
+  );
+
+  // ALU
+  alu alu (
+    .a      (alu_a_value),
+    .b      (alu_b_value),
+    .func   (func3),
+    .sub_sra(sub_sra),
+    .s      (alu_o),
+    .eq     (eq),
+    .lu     (lu),
+    .ls     (ls)
+  ); 
 
 endmodule
