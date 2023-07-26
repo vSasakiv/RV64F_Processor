f247615cd6d8b6373034ab7a232226543b7712a3
Vitor Sasaki <63128590+vSasakiv@users.noreply.github.com>
2023-07-26 20:44:35 +0000

    :sparkles: update simple modules to sv

--- testbenches/or64b_tb.sv
+++ testbenches/or64b_tb.sv
@@ -1,32 +1,30 @@
 `timescale 1ns / 100ps
-/* Testbench para o módulo AND de 32bits
-Para todos dois números entre 0 e 255, faz um bitwise AND deles e compara o resultado com a saída And do módulo.
+/* Testbench para o módulo OR de 32bits
+Para todos dois números entre 0 e 255, faz um bitwise OR deles e compara o resultado com a saída O do módulo.
 Se algum valor for diferente do esperado ("xpect"), mostra os valores na saída e aumenta a contagem do erros.
 Ao final, mostra a quantidade total de erros obtidos */
-module and64b_tb ();
-
+module or64b_tb ();
 logic [63:0] a, b, correct_s, s;
 int errors, i, j;
 
 // módulo testado
-and64b UUT (.a, .b, .s);
+or64b UUT (.a, .b, .s);
 
 initial begin
     errors = 0;
-    
+
     // geramos números aleatórios para serem testados
     for (i = 0; i < 1000; i = i + 1) begin
         a = {$urandom, $urandom};
         b = {$urandom, $urandom};
-        correct_s = a & b;
+        correct_s = a | b;
         #10
-        assert(s === correct_s) else begin 
-        errors += 1; 
-        $display ("Error A: %64b, B: %64b, expected %64b, got And: %64b", a, b, correct_s, s);
+        assert (s === correct_s) else begin
+            errors += 1;
+            $display ("Error A: %64b, B: %64b, expected %64b, got: %64b", a, b, correct_s, s);
         end
     end
-
-    $display("Finished. Got %d errors", errors);
+    $display ("Finished, got %2d errors", errors);
     $finish;
 end
 
--- testbenches/xor64b_tb.sv
+++ testbenches/xor64b_tb.sv
@@ -1,15 +1,13 @@
 `timescale 1ns / 100ps
-/* Testbench para o módulo AND de 32bits
-Para todos dois números entre 0 e 255, faz um bitwise AND deles e compara o resultado com a saída And do módulo.
+/* Testbench para o módulo XOR de 32bits
+Para todos dois números entre 0 e 255, faz um bitwise XOR deles e compara o resultado com a saída X do módulo.
 Se algum valor for diferente do esperado ("xpect"), mostra os valores na saída e aumenta a contagem do erros.
 Ao final, mostra a quantidade total de erros obtidos */
-module and64b_tb ();
-
+module xor64b_tb ();
 logic [63:0] a, b, correct_s, s;
-int errors, i, j;
-
+integer errors, i, j;
 // módulo testado
-and64b UUT (.a, .b, .s);
+xor64b UUT (.a, .b, .s);
 
 initial begin
     errors = 0;
@@ -18,15 +16,14 @@ initial begin
     for (i = 0; i < 1000; i = i + 1) begin
         a = {$urandom, $urandom};
         b = {$urandom, $urandom};
-        correct_s = a & b;
+        correct_s = a ^ b;
         #10
-        assert(s === correct_s) else begin 
-        errors += 1; 
-        $display ("Error A: %64b, B: %64b, expected %64b, got And: %64b", a, b, correct_s, s);
+        assert (s === correct_s) else begin 
+            errors += 1; 
+            $display ("Error A: %64b, B: %64b, expected %64b, got X: %64b", a, b, correct_s, s);
         end
     end
-
-    $display("Finished. Got %d errors", errors);
+    $display ("Finished, got %2d errors", errors);
     $finish;
 end
 
